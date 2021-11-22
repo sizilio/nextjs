2e9474684bda696f0a7a227775aea6de55033488
Vinícius Sizílio <vinicius.sizilio@gmail.com>
2021-11-22 18:14:26 -0300

    updates

--- src/components/content.js
+++ src/components/content.js
@@ -1,26 +1,32 @@
-import { Container, Row, Col, Navbar, Nav, NavDropdown, Form, FormControl, Button, Card } from 'react-bootstrap'
 import Styles from '../../styles/scss/content.module.scss'
 import Image from 'next/image'
 
 export default function Content() {
     return (
-        <div className="container mt-4 mt-md-5 mb-4 mb-md-5 p-3 p-md-5 d-flex justify-content-center">
-            <div className="card p-5">
-                <div className="d-flex flex-column justify-content-center align-items-center">
-                    <Image
-                        src="/vinicius-sizilio.jpg"
-                        alt="Vinícius Sizílio"
-                        width={120}
-                        height={120}
-                        className={Styles.image}
-                    />
-                    <h1 className={Styles.name + " mt-3"}>Vinícius Sizílio</h1>
-                    <h2 className={Styles.idd}>Desenvolvedor FullStack</h2>
-                    <span className={Styles.email}>vinicius.sizilio@gmail.coom</span>
-                    <div className="mt-4"> 
-                        <span className={Styles.label}>PHP</span>
-                        <span className={Styles.label}>Flutter</span>
-                        <span className={Styles.label}>NextJS</span>
+        <div className="container mt-2 mt-md-5 mb-4 mb-md-5 p-3 p-md-5">
+            <div className="row">
+                <div className="col-12 col-md-4 offset-md-4">
+                    <div className="card shadow">
+                        <div className={Styles.index + " pt-5 pb-4 px-5 text-center shadow"}>
+                            <Image
+                                src="/vinicius-sizilio.jpg"
+                                alt="Vinícius Sizílio"
+                                width={120}
+                                height={120}
+                                className={Styles.image}
+                            />
+                            <h1 className={Styles.name + " mt-3"}>Vinícius Sizílio</h1>
+                            <h2 className={Styles.idd}>Desenvolvedor FullStack</h2>
+                            <p className={Styles.email}>vinicius.sizilio@gmail.com</p>
+                            <a href="https://www.linkedin.com/in/vinicius-sizilio/" className="btn btn-secondary">
+                                LinkedIn
+                            </a>
+                        </div>
+                        <div className={Styles.label}>
+                            <span>PHP</span>
+                            <span>NextJS</span>
+                            <span>Flutter</span>
+                        </div>
                     </div>
                 </div>
             </div>
